c87fe2ea79670c696d2b20476aefff199d975ec3
ubu2 <ubu2@m-ubu2>
2015-04-29 18:19:34 +0200

    Versio 0.1 del projecte prjava01-Mateo15

---
+++ src/prjava01/mateo15/Prjava01Mateo15.java
@@ -0,0 +1,25 @@
+package prjava01.mateo15;
+
+import java.awt.Desktop;
+import java.io.*;
+
+public class Prjava01Mateo15 {
+
+public static void main(String[] args) throws IOException {
+        File f = new File("prj01-Mateo15.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web Jordi Mateo");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+    
+}
